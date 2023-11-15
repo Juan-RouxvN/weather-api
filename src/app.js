aeba5f248ccc00ff00ddc293b74a45252ae5bb3d
JRvNiekerk <b.jrtvn@gmail.com>
2023-11-15 12:14:05 +0200

    Finalized JS again

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const weatherRouter = require('./routes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
 
 const app = express();
 
 app.use(express.json());
 app.use(weatherRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
